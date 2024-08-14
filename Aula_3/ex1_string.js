2d8c2b7c1c2184926eb54df2410fca3428d5cde2
Luiz Eduardo <luiz131998@gmail.com>
2024-08-14 19:25:44 -0300

    Adicionei conteúdo ao ex1 da aula 3

--- Aula_3/ex1_string.js
+++ Aula_3/ex1_string.js
@@ -1,5 +1,12 @@
 const estudante = "Luiz";
 let num = 5;
-let texto = estudante + num;
+let texto = estudante + " " + num;
+let stexto = `nome ${estudante} numero ${num}.`; // Template String
 
-console.log()
+console.log(stexto);
+
+console.log(`A variavel "estudante" com valor "${estudante}" é do tipo:`, typeof(estudante));
+console.log(`A variavel "num" com valor "${num}" é do tipo:`, typeof(num));
+console.log(`A variavel "texto" com valor "${texto}" é do tipo:`, typeof(texto));
+console.log(`A variavel "stexto" com valor "${stexto}" é do tipo:`, typeof(stexto));
+
