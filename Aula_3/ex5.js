93049b2ed51d4fb6b94459985446fdf03e18860a
Luiz Eduardo <luiz131998@gmail.com>
2024-08-14 20:23:50 -0300

    Adicionei o arquivo ex5

---
+++ Aula_3/ex5.js
@@ -0,0 +1,24 @@
+const num = "5";
+const valor = null;
+const valor2 = undefined;
+const price = "100.5";
+let res = parseFloat(price) + 50;
+let res1 = Number(price) + 50;
+const p2 = "50.7";
+
+console.log(parseInt(num));
+console.log(Number(valor));
+console.log(Number(valor2));
+console.log(parseFloat(price));
+console.log(res);
+console.log(res1);
+console.log(Number(num));
+console.log(parseFloat(p2));
+
+
+
+
+
+
+
+
