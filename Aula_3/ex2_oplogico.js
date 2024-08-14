4ba5ad023bb26a13c5ffe5f7e7e99b413fd071e8
Luiz Eduardo <luiz131998@gmail.com>
2024-08-14 19:52:13 -0300

    Adicionei operadores logicos no arquivo ex2

--- Aula_3/ex2_oplogico.js
+++ Aula_3/ex2_oplogico.js
@@ -1,2 +1,18 @@
-const nota = 8;
-const falta = 4;
+const nota = 5;
+const falta = 8;
+
+if (nota > 5 || falta < 7) {
+    console.log("Aprovado");
+} else {
+    console.log("Reprovado");
+}
+
+
+
+
+
+
+
+
+
+
