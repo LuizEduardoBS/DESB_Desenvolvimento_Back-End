5715c2eb16f57ac6fbc9135ba8828acfef08faf4
Luiz Eduardo <luiz131998@gmail.com>
2024-08-14 20:38:59 -0300

    Adicionei o arquivo ex7

---
+++ Aula_3/ex7.js
@@ -0,0 +1,6 @@
+const texto = "senai backend aula 03";
+let stexto = texto.toUpperCase(); // Converte o texto para tudo maiúsculo.
+const texto2 = "SENAI BACKEND AULA 03";
+
+console.log(stexto);
+console.log(texto2.toLowerCase()); // Converte o texto tudo para minúsculo.
