87a5f94a2a0a1b95a11217d5b1e0e201f1076752
Luiz Eduardo <luiz131998@gmail.com>
2024-08-21 19:48:27 -0300

    Finalizei o ex1 da aula 4

--- Aula_4/ex1.js
+++ Aula_4/ex1.js
@@ -1,27 +1,61 @@
-// Cria uma classe chama Casa
+// cria uma classe chamada casa
 class Casa{
+    // cria o construtor que serve para passar parâmetros para a classe
     constructor(){
-        // Cria o construtor que serve para passar parâmetros para a classe
-        this.cor = null; // Parâmetro cor
-        this.qtdecomodos = null; // Parâmetro qtde de comodos
+        this.cor= null; // parametro de cor
+        this.qtdecomodos=null; // parametro de qtde de comodos 
     }
-    // Método para exibir informações
-    exibeinfo(cor,qtdecomodos) {
-        console.log(`A cor da casa é ${cor} e tem ${qtdecomodos} comodos.`);
+       // metódo para exibir as informações da casa
+        exibeinfo() {
+        console.log(`A cor da casa é ${this.cor} e tem ${this.qtdecomodos} comodos`);
+        
     }
+    // método abrir janela
+    abrirjanelas(qtde){
+        console.log(`Abrir ${qtde} janelas`);
+
+    }
+    fecharjanelas(qtde){
+        if(qtde ==2){
+            console.log("Todas as janelas estão fechadas");
+        }
+        if(qtde>2){
+               console.log(`Não existe ${qtde} janelas para serem fechadas`);
+        }
+        else{
+         console.log(`Fechada ${qtde} janelas`);
+        }
+        
+    }
+    abrirportas(){
+        console.log("Portas abertas");
+    }
+    fecharportas(){
+        console.log("Porta fechada");
+    }
+
 }
-
 // ESSA FUNÇÃO SERVE PARA MODULARIZAR O CÓDIGO
-// function main(){
-//     let minhaCasa = new Casa(); // Cria um objeto chamado minhaCasa
-//     minhaCasa.cor = "Azul";
-//     minhaCasa.qtdecomodos = 3;
-//     minhaCasa.exibeinfo(minhaCasa.cor, minhaCasa.qtdecomodos);
-// }
+/*
+function main(){
+    let minhaCasa = new Casa(); // cria um objeto chamado minha casa
+    minhaCasa.cor = "Azul";
+    minhaCasa.qtdecomodos= 3;
+    minhaCasa.exibeinfo(minhaCasa.cor,minhaCasa.qtdecomodos);
+}
+main();
+*/
+let minhaCasa = new Casa(); // cria um objeto chamado minha casa
+minhaCasa.cor = "Amarela";
+minhaCasa.qtdecomodos= 4;
+minhaCasa.exibeinfo();
 
-// main()
+const Casa2 = new Casa();
+Casa2.cor = "Vermelha";
+Casa2.qtdecomodos = 5;
+Casa2.exibeinfo();
+Casa2.abrirjanelas(1);
+Casa2.abrirportas();
+Casa2.abrirjanelas(2);
+Casa2.fecharjanelas(2);
 
-let minhaCasa = new Casa(); // Cria um objeto chamado minhaCasa
-minhaCasa.cor = "Azul";
-minhaCasa.qtdecomodos = 3;
-minhaCasa.exibeinfo(minhaCasa.cor, minhaCasa.qtdecomodos);
