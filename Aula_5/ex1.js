a22f6986b2d2a4f32648ed357a13ba7b20331a3c
Luiz Eduardo <luiz131998@gmail.com>
2024-08-28 19:07:57 -0300

    Adicionei a aula 5 e o exemplo 1

---
+++ Aula_5/ex1.js
@@ -0,0 +1,11 @@
+const frutas = ['Pera', 'Uva', 'Maça'];
+
+console.log(frutas);
+console.log(frutas[2]);
+
+const frutas2 = new Array('Maça', 'Morango', 'Laranja'); // Outra forma de criar um array
+
+console.log(frutas2);
+console.log(frutas2[1]);
+
+
