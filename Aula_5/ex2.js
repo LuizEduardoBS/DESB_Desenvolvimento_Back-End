a38bab43e137a09245bd917d473f0bf0c464d5ae
Luiz Eduardo <luiz131998@gmail.com>
2024-08-28 19:14:09 -0300

    Adicionei o ex2

---
+++ Aula_5/ex2.js
@@ -0,0 +1,12 @@
+notas = [5, 10, 7.5];
+console.log(notas);
+
+notas.pop(); // Remove o último elemento
+console.log(notas);
+notas.pop();
+console.log(notas);
+
+notas.push(9);
+console.log(notas);
+notas.push(10);
+console.log(notas);
