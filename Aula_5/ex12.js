622b8a18f90c54d96d2552f6a6e818864fd5030b
Luiz Eduardo <luiz131998@gmail.com>
2024-08-28 20:44:19 -0300

    Adicionei uma funcao no ex12 da aula 5

--- Aula_5/ex12.js
+++ Aula_5/ex12.js
@@ -3,4 +3,10 @@ carro.modelo = "Jaguar"
 carro.ano = 2024
 carro.combustivel = "Hibrido"
 carro.portas = 5
+carro.acelera = function() {
+    return "O carro está acelerando"
+}
+
+
 console.log(carro);
+console.log(carro.acelera())
