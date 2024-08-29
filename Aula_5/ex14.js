bef6bb63db145cfd761f9a733e755483ebe687a6
Luiz Eduardo <luiz131998@gmail.com>
2024-08-28 21:26:52 -0300

    Adicionei o ex14 na aula 5

---
+++ Aula_5/ex14.js
@@ -0,0 +1,12 @@
+let usuario = {
+    nome : 'Luiz',
+    idade : '25',
+    isAdmin : true
+}
+
+if(usuario.isAdmin === true) {
+    console.log("O usuário é o administrador.")
+}
+else {
+    console.log("o usuário não é o administrador.")
+}
