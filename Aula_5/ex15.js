4920f090c3c4229d8511dc92b7081220407cd367
Luiz Eduardo <luiz131998@gmail.com>
2024-08-28 21:31:12 -0300

    Adicionei o ex15 na aula 5

---
+++ Aula_5/ex15.js
@@ -0,0 +1,10 @@
+const dadoP = {
+    nome : 'Luiz',
+    idade : 25,
+    endereco : {
+        rua : 'Av. UmDoiTres',
+        numero : 123
+    }
+}
+console.log(dadoP);
+console.log(dadoP.endereco.rua);
