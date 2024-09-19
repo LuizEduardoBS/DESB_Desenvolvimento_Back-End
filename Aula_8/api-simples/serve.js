39cc1eec1c6dba13eb6525d49141a77bf58ff5bd
Luiz Eduardo <luiz131998@gmail.com>
2024-09-18 21:30:30 -0300

    At

--- Aula_8/api-simples/serve.js
+++ Aula_8/api-simples/serve.js
@@ -1,8 +1,10 @@
 const express = require('express'); // Importa o Express
 
-const app = express() // Cria a instância
+const app = express(); // Cria a instância
 
 const port = 3000; // Define a porta onde a api irá rodar
+
+const dados = []; // Lista para armazenar todos os dados cadastrados
 
 app.use(express.json()); // Middleware para permitir que o servidor lide com requisições no formato json
 
@@ -12,6 +14,15 @@ app.get('/', (req, res) => {
 })
 
 app.post('/data', (req, res) => {
+    // Parse para envio dos dados
+    const { nome, idade } = req.body;
+    res.send(`Nome: ${nome}, Idade: ${idade}`);
+    dados.push({nome, idade}); // Adiciona na lista
+})
+
+// Método data para o end point data
+app.get('/data', (req, res) => {
+    // Parse para leitura dos dados
     const { nome, idade } = req.body;
     res.send(`Nome: ${nome}, Idade: ${idade}`);
 })
