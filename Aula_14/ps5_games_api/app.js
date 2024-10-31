80003b02f0c227056b4fc6d540ee24c104131840
Luiz Eduardo <luiz131998@gmail.com>
2024-10-30 21:38:37 -0300

    Adicionei a aula 14

---
+++ Aula_14/ps5_games_api/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParse = require('body-parser');
+const gamesRoutes = require('./routes/games');
+const path = require('path');
+
+const app = express();
+
+// Conecta ao MongoDB
+mongoose.connect('mongodb+srv://luized:Luiz123@cluster0.u7srm.mongodb.net/ps5_games', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log('Conectado ao MongoDB'))
+    .catch(err => console.log(err));
+
+// Middleware
+app.use(bodyParse.json());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+// Rotas
+app.use('/api/games', gamesRoutes);
+
+// Inicia o servidor
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
